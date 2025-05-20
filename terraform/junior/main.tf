c2f979e9e96ea7be097b0a4213f4dde3601f3a81
Chaitanya Korepu <chaitanya772@gmail.com>
2025-05-20 19:45:40 +0100

    added the main.tf file and removed unwanted tfe files

--- terraform/junior/main.tf
+++ terraform/junior/main.tf
@@ -1,47 +1,25 @@
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
+# resource to create gcs bucket with name and random unique id
+resource "google_storage_bucket" "db_junior_gcs_bucket" {
   name     = "test-bucket-${random_id.bucket_suffix.hex}"
-  #  name     = "junior-bucket-${var.project_id}" #with project id
-  # name     = "junior-bucket-${var.project_id}-${random_id.bucket_suffix.hex}" with both random_id and project_id
+  # name     = "junior-bucket-${var.project_id}"   #with project id
+  # name     = "junior-bucket-${var.project_id}-${random_id.bucket_suffix.hex}"  #with both random_id and project_id
   location = var.region
   force_destroy = true
-
   uniform_bucket_level_access = true
-
   labels = {
     environment = "dev"
     managed_by  = "terraform"
   }
 }
 
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-  default = "435094159146"
-}
-
-# variable "project_name" {
-#   description = "The GCP project ID"
-#   type        = string
-#   default = "My playground"
-# }
-
-variable "region" {
-  description = "The GCP region to create the bucket in"
-  type        = string
-  default     = "europe-west3"
-}
-
-output "bucket_name" {
-  description = "The name of the created GCS bucket"
-  value       = google_storage_bucket.gcs_bucket.name
+# this will create an object with name config/settings.txt
+resource "google_storage_bucket_object" "object_settings_file" {
+  name   = "config/settings.txt"
+  bucket = var.bucket_name
+  content = "Hello from Terraform!"  #this is the content inside the file
 }
