aad0582af07e9c34bafce2f8c58922ac23ec2dcc
Chaitanya Korepu <chaitanya772@gmail.com>
2025-05-27 12:51:15 +0100

    added location=global for self_signed_ca

--- terraform/INT3/main.tf
+++ terraform/INT3/main.tf
@@ -79,7 +79,7 @@ resource "google_privateca_ca_pool" "ca_pool" {
 }
 
 resource "google_privateca_certificate_authority" "self_signed_ca" {
-  location                  = "us-central1"
+  location                  = "global"
   pool                      = google_privateca_ca_pool.ca_pool.name
   certificate_authority_id  = "self-signed-ca"
   type                      = "SELF_SIGNED"
